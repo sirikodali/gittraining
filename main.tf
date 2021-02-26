09227797699f533de890476b0d5de518f01d789e
sirikodali <52023644+sirikodali@users.noreply.github.com>
2021-02-26 22:38:33 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
    region   = "ap-south-1"
-   access_key = "AKIAU4GQFG4YD2UL53M3"
-   secret_key = "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"
+   access_key = "************"
+   secret_key = "********+************"
 }
 
 resource "aws_s3_bucket" "mybucket" {
